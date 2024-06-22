113e24e9fe5f0ce18df09270a0abe6580359f16c
Muhammad Faizan <mf2028136@gmail.com>
2024-06-22 14:27:50 +0500

    Hero section completed

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/images/hero_img.jpg')", zIndex: "-100" }}>
+      <Navbar/>
+
+      <HeroSection/>
+
     </div>
   )
 }
